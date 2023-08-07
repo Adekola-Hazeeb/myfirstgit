10527343bad2d21809968fa0af7e4f10fdaee808
Adekola-Hazeeb <Adekola-Hazeeb>
2023-08-07 16:57:07 +0100

    hit git

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿//Console.WriteLine("Hello, World!");
+// calculate cal = new calculate();
+// int result=cal.addition(10,20);
+// Console.WriteLine(result);
+// public class calculate 
+// {
+    // public int addition(int a ,int b)
+    // {
+    //     int c = a + b;
+    //     return c;
+    // }
+// }
+// test ts =new test();
+// Console.WriteLine(ts.i+4);
+// public class test 
+// {
+//   public int i ;
+// }
+// explicitj f1v=new explicitj();
+// System.Console.WriteLine(f1v);
+// public class explicitj
+// {
+//     public explicitj()
+//     {
+//         System.Console.WriteLine("explicit constructor is called ");
+//     }
+// }
+exam exa = new exam();
+if (exa.v==0)
+{
+    System.Console.WriteLine("it has been taken care of");
+}
+// System.Console.WriteLine(exa.r);
+// System.Console.WriteLine(exa.v);
+// System.Console.WriteLine(exa.b);
+public class exam
+{
+    public int v;
+    public string r;
+    public bool b ;
+}
